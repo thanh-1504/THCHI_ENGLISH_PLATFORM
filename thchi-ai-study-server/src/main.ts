6f68953dd92f4ada81d4712e99854f12fe730169
nhatthanh <duongnhatthanhaz09@gmail.com>
2026-09-23 13:37:32 +0700

    Handle bootstrap NestJS application with CORS and cookie-parser

--- thchi-ai-study-server/src/main.ts
+++ thchi-ai-study-server/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://thchi.netlify.app/',
+    origin: 'https://thchi.netlify.app',
     credentials: true,
   });
   app.use(cookieParser());
