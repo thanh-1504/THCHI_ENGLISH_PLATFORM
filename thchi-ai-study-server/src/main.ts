42c054aa2199fbe36c6797b491189c43e53622f0
nhatthanh <duongnhatthanhaz09@gmail.com>
2026-09-23 13:06:11 +0700

    Change Cor in main.ts

--- thchi-ai-study-server/src/main.ts
+++ thchi-ai-study-server/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://thchi.netlify.app/',
     credentials: true,
   });
   app.use(cookieParser());
